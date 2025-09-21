dbc8b02c6a66a39865a21c1cb7d33e524eae5318
Mansi08git <mansiworks08@gmail.com>
2025-09-21 21:29:00 +0530

    Added the agent, task, and tool

--- Agents/legal_precedents_agent.py
+++ Agents/legal_precedents_agent.py
@@ -1,17 +1,23 @@
 #3rd Legal Precedent Agent
 
 from crewai import Agent, LLM
+from tools.legal_precedent_search_tool import search_legal_precedents
+
 
 llm = LLM(
-    model="",
-    temperature=
+    model="groq/llama-3.3-70b-versatile",
+    temperature=0
     )
 
 precendent_agent = Agent(
-    role="",
-    goal="",
-    backstory="",
+    role="Legal Precedent Agent",
+    goal="Find relevant legal precedent cases based on the user's legal issue",
+    backstory=(
+        "You're an expert legal researcher who specializes in finding case law and precedent judgements."
+        "You're skilled in identifying relevant case summaries based on natural language descriptions of legal issues."
+        "Your task is to search trusted legal databases to support legal analysis with past judgements."
+    ),
     llm =llm,
-    tools ="",
+    tools =[search_legal_precedents],
     verbose=True
 )
---
+++ tools/ipc_sections_search_tool.py
@@ -0,0 +1,46 @@
+#ipc_sections_search_tool.py 
+#import libraries
+#  
+import os
+
+from dotenv import load_dotenv
+from crewai.tools import tool 
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings 
+
+
+@tool("IPC Sections Search Tool")
+def search_ipc_sections(query:str) -> list[dict]:
+    load_dotenv()
+    persist_dir = os.getenv("PERSIST_DIRECTORY_PATH")
+    if not persist_dir:
+        raise EnvironmentError("Persist directory path not found")
+    
+    collection_name = os.getenv("IPC_COLLECTION_NAME")
+    embedding = HuggingFaceEmbeddings()
+
+    #Load Vector Store
+    Vectordb = Chroma(
+        collection_name=collection_name,
+        embedding_function=embedding,
+        persist_directory= persist_dir
+    )
+
+    top_k= 3 # argument for flexibility 
+
+    # similarity search
+    docs = Vectordb.similarity_search(query, top_k)
+
+    #Format Result 
+    return [
+        {
+            "section": doc.metadata.get("section"),
+            "section_title": doc.metadata.get("section_title"),
+            "chapter":doc.metadata.get("chapter"),
+            "chapter_title":doc.metadata.get("chapter_title"),
+            "content":doc.page_content
+        }
+        for doc in docs
+    ]
+
+
